3e88f1f54aef31168e43e3f4d4d9bdac702157dc
Ty Thompson <tylermoko@gmail.com>
2019-05-28 21:25:30 -0400

    adding qty select

--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -2,8 +2,9 @@
   <v-container>
     <v-layout>
       <draggable v-model="foodList">
-      <v-flex :key="food.name" v-for="food in foodList">
+      <v-flex class="food-item" style="border:1px solid green;" :key="food.name" v-for="food in foodList">
         {{food.name}}
+        <v-select :items="qtySelect" v-model="food.qty" />
       </v-flex>
       </draggable>
     </v-layout>
@@ -22,14 +23,18 @@ import draggable from 'vuedraggable'
         {name: 'Orange', color: 'green', qty: 2},
         {name: 'Lettuce', color: 'green', qty:1},
         {name: 'Milk', color: 'yellow', qty: 1},
-      ]
+      ],
+      qtySelect: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     })
   }
 </script>
 
 <style>
-
+.food-item:hover{
+  cursor:move;
+}
 </style>
+
 
 --TO DO--
 ()
