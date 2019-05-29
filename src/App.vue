da7469a61b0da9c8aceac493490e6fcc2e7b6206
Ty Thompson <tylermoko@gmail.com>
2019-05-28 20:51:04 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <FoodList/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import FoodList from './components/FoodList'
+
+export default {
+  name: 'App',
+  components: {
+    FoodList
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/FoodList.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex :key="food.name" v-for="food in foodList">
+        {{food.name}}
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      foodList: [
+        {name: 'Apple', color: 'green', qty: 3},
+        {name: 'Orange', color: 'green', qty: 2},
+        {name: 'Lettuce', color: 'green', qty:1},
+        {name: 'Milk', color: 'yellow', qty: 1},
+      ]
+    })
+  }
+</script>
+
+<style>
+
+</style>
