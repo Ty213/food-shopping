57a5539ef4532d452ff44fb14fc02a83acae768c
Ty Thompson <tylermoko@gmail.com>
2019-05-30 17:13:23 -0400

    adding color picker

--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -5,6 +5,8 @@
       <v-flex class="food-item" style="border:1px solid green;" :key="food.name" v-for="food in foodList">
         {{food.name}}
         <v-select :items="qtySelect" v-model="food.qty" />
+        <v-btn flat @click="food.menu = true"><v-icon>palette</v-icon></v-btn>
+        <v-flex v-if="food.menu">colorheres</v-flex>
       </v-flex>
       </draggable>
     </v-layout>
@@ -19,10 +21,10 @@ import draggable from 'vuedraggable'
     },
     data: () => ({
       foodList: [
-        {name: 'Apple', color: 'green', qty: 3},
-        {name: 'Orange', color: 'green', qty: 2},
-        {name: 'Lettuce', color: 'green', qty:1},
-        {name: 'Milk', color: 'yellow', qty: 1},
+        {name: 'Apple', color: 'green', qty: 3, menu: false},
+        {name: 'Orange', color: 'green', qty: 2, menu: false},
+        {name: 'Lettuce', color: 'green', qty:1, menu: false},
+        {name: 'Milk', color: 'yellow', qty: 1, menu: false},
       ],
       qtySelect: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     })
