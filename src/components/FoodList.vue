468945b819bab9d440533999cc3a5759352cff56
Ty Thompson <tylermoko@gmail.com>
2019-05-30 20:39:51 -0400

    adding color palette

--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -2,12 +2,20 @@
   <v-container>
     <v-layout>
       <draggable v-model="foodList">
-      <v-flex class="food-item" style="border:1px solid green;" :key="food.name" v-for="food in foodList">
+      <v-card :color="food.color" class="food-item" :key="food.name" v-for="food in foodList">
         {{food.name}}
         <v-select :items="qtySelect" v-model="food.qty" />
         <v-btn flat @click="food.menu = true"><v-icon>palette</v-icon></v-btn>
-        <v-flex v-if="food.menu">colorheres</v-flex>
-      </v-flex>
+        <v-layout v-if="food.menu">
+          <v-flex><v-btn @click="food.color = '#4d5057'" color="#4d5057"></v-btn></v-flex>
+          <v-flex><v-btn @click.native="food.color = '#c33c54'" color="#c33c54"></v-btn></v-flex>
+          <v-flex><v-btn @click="food.color = '#4da167'" color="#4da167"></v-btn></v-flex>
+
+          <v-flex><v-btn @click="food.color = '#c6ca53'" color="#c6ca53"></v-btn></v-flex>
+          <v-flex><v-btn @click="food.color = '#058ed9'" color="#058ed9"></v-btn></v-flex>
+
+        </v-layout>
+      </v-card>
       </draggable>
     </v-layout>
   </v-container>
@@ -21,19 +29,28 @@ import draggable from 'vuedraggable'
     },
     data: () => ({
       foodList: [
-        {name: 'Apple', color: 'green', qty: 3, menu: false},
-        {name: 'Orange', color: 'green', qty: 2, menu: false},
-        {name: 'Lettuce', color: 'green', qty:1, menu: false},
-        {name: 'Milk', color: 'yellow', qty: 1, menu: false},
+        {name: 'Apple', color: 'green', qty: 3, menu: false, color: '#4d5057' },
+        {name: 'Orange', color: 'green', qty: 2, menu: false, color: '#4d5057'},
+        {name: 'Lettuce', color: 'green', qty:1, menu: false, color: '#4d5057'},
+        {name: 'Milk', color: 'yellow', qty: 1, menu: false, color: '#4d5057'},
       ],
       qtySelect: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    })
+    }),
+    methods: {
+      setFoodColor(food,color){
+        console.o
+      }
+    }
   }
 </script>
 
 <style>
 .food-item:hover{
   cursor:move;
+}
+.food-item{
+  color:white;
+  margin: .5rem;
 }
 </style>
 
